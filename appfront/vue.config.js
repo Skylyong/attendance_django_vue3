06409939ffcaa329762e3b44105660f71f4698f3
Skylyong <lyong_s@foxmail.com>
2022-03-19 04:02:06 +0800

    重新打包前端代码

--- appfront/vue.config.js
+++ appfront/vue.config.js
@@ -1,10 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
-// module.exports = defineConfig({
-//   transpileDependencies: true
-// },
+module.exports = defineConfig({
+  transpileDependencies: true
+},
 
-// )
+)
 
-module.exports = {
-  assetsDir: 'collect_static',// 静态资源打包输出目录 (js, css, img, fonts)，相应的url路径也会改变
-};
+// module.exports = {
+//   assetsDir: 'collect_static',// 静态资源打包输出目录 (js, css, img, fonts)，相应的url路径也会改变
+// };
