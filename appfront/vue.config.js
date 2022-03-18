decb81421d32401b4627ffdfb54aa579f6556e59
Skylyong <lyong_s@foxmail.com>
2022-03-19 03:47:13 +0800

    更改vue静态文件打包路径，并重新打包前端代码

--- appfront/vue.config.js
+++ appfront/vue.config.js
@@ -6,5 +6,5 @@ const { defineConfig } = require('@vue/cli-service')
 // )
 
 module.exports = {
-  assetsDir: 'static',// 静态资源打包输出目录 (js, css, img, fonts)，相应的url路径也会改变
+  assetsDir: 'collect_static',// 静态资源打包输出目录 (js, css, img, fonts)，相应的url路径也会改变
 };
